68ddbbbf0b4db70e0d4fcdaef536244acfd591e3
Susana vik <susana.m.vik@gmail.com>
2021-02-03 13:47:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,11 @@
 require 'pry'
 
 def start_game(player1 = "Rocky", player2 = "Bullwinkle")
-  binding.pry
   puts "Hello #{player1} & #{player2}"
   return start_game
 end
 
 def play_game(player1 = "Jay-z", player2 = "Beyonce")
-  binding.pry
   puts "#{player1} is better than #{player2}"
   return play_game
 end
