6fbf31d84f3bfcff1a9e010f846a53e00371d391
Susana vik <susana.m.vik@gmail.com>
2021-02-03 14:11:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    binding.pry
-    10.times "s" + string
+    10. * "s".to_i + string
   else
-    binding.pry
     string
   end
 end
