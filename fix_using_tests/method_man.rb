54bd92adec36a024cfca106479cbda38c0363d27
Susana vik <susana.m.vik@gmail.com>
2021-02-03 13:52:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,12 @@
 
 def start_game(player1 = "Rocky", player2 = "Bullwinkle")
   puts "Hello #{player1} & #{player2}"
-  return start_game
 end
+
+  return start_game
 
 def play_game(player1 = "Jay-z", player2 = "Beyonce")
   puts "#{player1} is better than #{player2}"
-  return play_game
 end
+
+return play_game
