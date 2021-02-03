711731789dd35fe81e97873c3653567634a0bb67
Susana vik <susana.m.vik@gmail.com>
2021-02-03 15:41:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,17 +1,14 @@
 require 'pry'
 
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  binding.pry
-  puts state_log(date)
+  state_log(date)
   date = generate_star_date
 end
