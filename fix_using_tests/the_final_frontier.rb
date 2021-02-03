6bbcbb0efcb9f5e66c1377bd2ba93a7cd84119de
Susana vik <susana.m.vik@gmail.com>
2021-02-03 16:23:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,13 +2,12 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-generate_star_date = star_date
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
+  star_date = generate_star_date
   state_log(star_date)
   generate_star_date
 end
